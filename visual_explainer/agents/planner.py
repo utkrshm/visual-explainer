3eaa7a00feaa951cdb6e0e4fde44d0ca0454f8f5
utkrshm <malaiya.utkarsh@gmail.com>
2026-02-17 03:52:13 +0530

    feat: add v1 implementation of the planner

---
+++ visual_explainer/agents/planner.py
@@ -0,0 +1,36 @@
+
+from typing import List
+
+from pydantic import BaseModel, Field
+
+from visual_explainer.state import Scene
+
+from .agent import BaseAgent
+from .prompts.planner import PLANNER_PROMPT
+
+
+class PlannerOutput(BaseModel):
+    scenes: List[Scene] = Field(description="The chronological list of scenes for the video.")
+
+class Planner(BaseAgent):
+    def __init__(self, client, model="openai/gpt-oss-20b"):
+        super().__init__(
+            llm_client=client,
+            model=model,
+            system_prompt=PLANNER_PROMPT,
+            agent_name="Planner",
+            output_schema=PlannerOutput
+        )
+        
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    from groq import Groq
+    load_dotenv()
+    
+    client = Groq()
+    planner = Planner(client)
+    output: PlannerOutput = planner.invoke([
+        {"role": "user", "content": "Explain the concept of 'Pythagoras theorem'"}
+    ])
+    
+    print(output.model_dump_json())
